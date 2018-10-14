91b0029a77fbe8fe5c6a79de814c0824891c0be0
Harry Scells <harryscells@gmail.com>
2018-10-15 09:39:48 +1000

    Make ws handler public.

---
+++ websocket.go
@@ -0,0 +1,46 @@
+package bigbro
+
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+	"time"
+)
+
+// Upgrader upgrades a web socket.
+var Upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+// WsEvent handles read events from the web socket.
+func WsEvent(ws *websocket.Conn, l Logger) {
+
+	readWait := 1 * time.Millisecond
+	readTicker := time.NewTicker(readWait)
+
+	// defer closing of web socket
+	defer func() {
+		readTicker.Stop()
+		ws.Close()
+	}()
+
+	for {
+		select {
+		case <-readTicker.C:
+			var event Event
+			err := ws.ReadJSON(&event)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			err = l.Log(event)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+		}
+	}
+}

ba485886157eaf91b7910144e067d69f6986e0ff
Harry Scells <harryscells@gmail.com>
2019-08-06 10:12:32 +1000

    Do not check origin when upgrading.

--- serve.go
+++ serve.go
@@ -2,17 +2,11 @@ package bigbro
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
 	"log"
 )
 
-// upgrader upgrades a web socket.
-var upgrader = websocket.Upgrader{
-	ReadBufferSize: 1024,
-}
-
 func (l Logger) GinEndpoint(c *gin.Context) {
-	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	ws, err := Upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
 		log.Println(err)
 		return

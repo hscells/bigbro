24c19482aa63d87822801315bb2f297320568eb4
Harry Scells <harryscells@gmail.com>
2019-08-06 10:09:57 +1000

    Do not overwrite ws error.

--- serve.go
+++ serve.go
@@ -3,19 +3,18 @@ package bigbro
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
-	"net/http"
+	"log"
 )
 
 // upgrader upgrades a web socket.
 var upgrader = websocket.Upgrader{
 	ReadBufferSize: 1024,
-
 }
 
 func (l Logger) GinEndpoint(c *gin.Context) {
 	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
-		c.String(http.StatusInternalServerError, err.Error())
+		log.Println(err)
 		return
 	}
 
